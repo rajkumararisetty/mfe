76e4ceb2f51b7daa9899d1ad87e7e912226b0254
Rajkumar Arisetty <rajkumar.arisetty@preqin.com>
2021-08-16 17:43:48 +0530

    Setup basic ui auth and created auth deployment script

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Header from './components/Header'; 
@@ -10,13 +10,16 @@ const AuthLazy = lazy(() => import('./components/AuthApp'));
 const generateClassName = createGenerateClassName({ productionPrefix: 'co' });
 
 export default () => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
     return (
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
-                <Header />
+                <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
                 <Suspense fallback={<Progress />}>
                     <Switch>
-                        <Route path="/auth" component={AuthLazy} />
+                        <Route path="/auth">
+                            <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                        </Route>
                         <Route path="/" component={MarketingLazy} />
                     </Switch>
                 </Suspense>
