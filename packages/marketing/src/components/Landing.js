6a95385137e228133586ff4585cf9a95c334db68
Rajkumar Arisetty <rajkumar.arisetty@preqin.com>
2021-08-19 15:54:07 +0530

    marketing update

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Home Page
+              Home Pages
             </Typography>
             <Typography
               variant="h5"
